8b1b8cac16ac2cc6bfa993feee692aff66922ae6
scalette <furca7e@gmail.com>
2024-05-04 10:11:20 +0200

    feat: page controller

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -1,32 +1,72 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	NotFoundException,
+	Param,
+	Patch,
+	Post,
+	UseGuards,
+	UsePipes,
+	ValidationPipe
+} from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
+import { IdValidationPipe } from '../pipes/id-validation.pipe';
 import { FindTopPageDto } from './dto/find-top-page.dto';
-import { TopPageModel } from './top-page.model';
+import { TOP_PAGE_NOT_FOUND_ERROR } from './top-page.constants';
+import { TopPageService } from './top-page.service';
+import { CreateTopPageDto } from './dto/create-top-page.dto';
 
 @Controller('top-page')
 export class TopPageController {
-	@Post('create')
-	async create(@Body() dto: Omit<TopPageModel, '_id'>) {
+	constructor(private readonly topPageService: TopPageService) { }
 
+	@Post('create')
+	async create(@Body() dto: CreateTopPageDto) {
+		return this.topPageService.create(dto)
 	}
 
 	@Get(':id')
 	async get(@Param('id') id: string) {
+		const page = await this.topPageService.findById(id);
+		if (!page) {
+			throw new NotFoundException(TOP_PAGE_NOT_FOUND_ERROR)
+		}
+		return page
+	}
 
+	@Get('byAlias/:alias')
+	async getByAlias(@Param('alias') alias: string) {
+		const page = await this.topPageService.findByAlias(alias);
+		if (!page) {
+			throw new NotFoundException(TOP_PAGE_NOT_FOUND_ERROR)
+		}
+		return page
 	}
 
 	@Delete(':id')
-	async delete(@Param('id') id: string) {
-
+	async delete(@Param('id', IdValidationPipe) id: string) {
+		const deletedPage = await this.topPageService.deleteById(id);
+		if (!deletedPage) {
+			throw new NotFoundException(TOP_PAGE_NOT_FOUND_ERROR)
+		}
 	}
 
 	@Patch(':id')
-	async patch(@Param('id') id: string, @Body() dto: TopPageModel) {
-
+	async patch(@Param('id', IdValidationPipe) id: string, @Body() dto: CreateTopPageDto) {
+		const updatedPage = await this.topPageService.updateById(id, dto);
+		if (!updatedPage) {
+			throw new NotFoundException(TOP_PAGE_NOT_FOUND_ERROR)
+		}
+		return updatedPage
 	}
 
+	@UsePipes(new ValidationPipe())
 	@HttpCode(200)
-	@Post()
+	@Post('find')
 	async find(@Body() dto: FindTopPageDto) {
-
+		return this.topPageService.findByCategory(dto.firstCategory)
 	}
 }
---
+++ src/top-page/top-page.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, UseGuards } from '@nestjs/common';
+import { InjectModel } from 'nestjs-typegoose';
+import { TopLevelCategory, TopPageModel } from './top-page.model';
+import { ModelType } from '@typegoose/typegoose/lib/types';
+import { CreateTopPageDto } from './dto/create-top-page.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+
+@Injectable()
+export class TopPageService {
+  constructor(@InjectModel(TopPageModel) private readonly topPageModel: ModelType<TopPageModel>) { }
+
+  @UseGuards(JwtAuthGuard)
+  async create(dto: CreateTopPageDto) {
+    return this.topPageModel.create(dto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  async findById(id: string) {
+    return this.topPageModel.findById(id).exec();
+  }
+
+  async findByAlias(alias: string) {
+    return this.topPageModel.findOne({ alias }).exec()
+  }
+
+  @UseGuards(JwtAuthGuard)
+  async deleteById(id: string) {
+    return this.topPageModel.findByIdAndDelete(id).exec()
+  }
+
+  @UseGuards(JwtAuthGuard)
+  async updateById(id: string, dto: CreateTopPageDto) {
+    return this.topPageModel.findByIdAndUpdate(id, dto, { new: true }).exec()
+  }
+
+  async findByCategory(firstCategory: TopLevelCategory) {
+    return this.topPageModel.find({ firstCategory }, { alias: 1, secondCategory: 1, title: 1 }).exec()
+  }
+}
